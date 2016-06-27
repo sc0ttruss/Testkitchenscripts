3f51e9da77df52bcf51bb46fddf882f2430e7084
Scott <sr@chef.io>
2016-06-27 16:15:01 +0300

    remove duplicate install of push-jobs-server

--- installdelivery.sh
+++ installdelivery.sh
@@ -31,7 +31,6 @@ wget -N https://packages.chef.io/stable/el/7/opscode-analytics-1.4.0-1.el7.x86_6
 wget -N https://packages.chef.io/stable/el/7/opscode-reporting-1.6.0-1.el7.x86_64.rpm --no-check-certificate
 wget -N https://packages.chef.io/stable/el/7/chef-server-core-12.6.0-1.el7.x86_64.rpm --no-check-certificate
 wget -N https://packages.chef.io/stable/el/7/push-jobs-client-1.3.4-1.el7.x86_64.rpm --no-check-certificate
-wget -N https://packages.chef.io/stable/el/6/opscode-push-jobs-server-1.1.6-1.x86_64.rpm --no-check-certificate
 wget -N https://packages.chef.io/stable/el/7/chefdk-0.14.25-1.el7.x86_64.rpm --no-check-certificate
 wget -N https://packages.chef.io/stable/el/7/delivery-0.4.299-1.el7.x86_64.rpm --no-check-certificate
 wget -N https://packages.chef.io/stable/el/7/opscode-reporting-1.6.0-1.el7.x86_64.rpm --no-check-certificate
